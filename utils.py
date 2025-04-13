7b1279f0ed0d282f145a9029b8f2a1badc852d45
Anubhav Bangari <bangarianubhav@gmail.com>
2025-04-13 13:39:52 +0530

    13th April

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import os
+import pysubs2
+import pandas as pd
 
 def load_subtitles_dataset(file_path):
     """
@@ -8,3 +11,21 @@ def load_subtitles_dataset(file_path):
     if 'script' not in df.columns:
         raise ValueError("CSV must contain a 'script' column.")
     return df
+
+
+def load_subtitles_datasetnew(folder_path):
+    data = []
+
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".ass"):
+            file_path = os.path.join(folder_path, filename)
+            subs = pysubs2.load(file_path)
+            for line in subs:
+                data.append({
+                    "episode": filename,
+                    "start": line.start,
+                    "end": line.end,
+                    "text": line.text
+                })
+
+    return pd.DataFrame(data)
